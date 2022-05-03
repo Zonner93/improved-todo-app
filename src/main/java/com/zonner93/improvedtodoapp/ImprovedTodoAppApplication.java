358a5d5b216627f072d53868cc34fcbba440e3f7
Adam Pawluczuk <adam.pawluczuk@ppuc.pl>
2022-05-03 12:59:46 +0200

    initial commit

---
+++ src/main/java/com/zonner93/improvedtodoapp/ImprovedTodoAppApplication.java
@@ -0,0 +1,13 @@
+package com.zonner93.improvedtodoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImprovedTodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImprovedTodoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zonner93/improvedtodoapp/ImprovedTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zonner93.improvedtodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImprovedTodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
