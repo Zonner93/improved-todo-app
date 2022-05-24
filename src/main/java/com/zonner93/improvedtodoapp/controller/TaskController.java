4886c4c73242d6e039f3946fc0ca9dcd25e6c690
Adam Pawluczuk <adam.pawluczuk@ppuc.pl>
2022-05-24 15:37:30 +0200

    sekcja 3 page

--- src/main/java/com/zonner93/improvedtodoapp/controller/TaskController.java
+++ src/main/java/com/zonner93/improvedtodoapp/controller/TaskController.java
@@ -1,12 +1,16 @@
 package com.zonner93.improvedtodoapp.controller;
 
+import com.zonner93.improvedtodoapp.model.Task;
 import com.zonner93.improvedtodoapp.model.TaskRepository;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @RepositoryRestController
 @RequiredArgsConstructor
@@ -15,8 +19,14 @@ public class TaskController {
     private final TaskRepository taskRepository;
 
     @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
-    ResponseEntity<?> readAllTasks(){
+    ResponseEntity<List<Task>> readAllTasks(){
         logger.warn("Exposing all the tasks.");
         return ResponseEntity.ok(taskRepository.findAll());
+    }
+
+    @GetMapping(value = "/tasks")
+    ResponseEntity<List<Task>> readAllTasks(Pageable page){
+        logger.warn("Custom pageable.");
+        return ResponseEntity.ok(taskRepository.findAll(page).getContent());
     }
 }
